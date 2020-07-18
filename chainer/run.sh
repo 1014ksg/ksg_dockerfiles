3c8c1e8c62195cd2bd3bd44f4ffec29c030b7e5b
1014ksg <1014ksg1@keio.jp>
2020-07-18 14:29:39 +0900

    add : firstcommit

---
+++ chainer/run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+xhost +local:user
+docker run -it \
+--runtime=nvidia \
+--env=DISPLAY=$DISPLAY \
+--privileged \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--env="QT_X11_NO_MITSHM=1" \
+--rm \
+-v "/etc/group:/etc/group:ro" \
+-v "/etc/passwd:/etc/passwd:ro" \
+-v "$(pwd)/../../action_conditional_pressure_predict:/action_conditional_pressure_predict" \
+-v "$(pwd)/../../make_dataset_GU:/make_dataset_GU" \
+--net host \
+ksg_chainer
